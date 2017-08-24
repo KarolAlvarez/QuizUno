9921827734d10e8617f2fdc857aa2736b14c49f0
karol <aalvarez3570@gmail.com>
2017-08-24 13:58:48 -0500

    ultimo cambio

--- app/src/main/java/com/i014114/quizuno/Main2Activity.java
+++ app/src/main/java/com/i014114/quizuno/Main2Activity.java
@@ -2,6 +2,10 @@ package com.i014114.quizuno;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +13,30 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+
+        Button mibotonUno= (Button) findViewById(R.id.mopciones);
+        registerForContextMenu(mibotonUno);
+
     }
+
+
+
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        MenuInflater menInflater= getMenuInflater();
+        menu.setHeaderTitle("mi menu de opciones");
+        menInflater.inflate(R.menu.mimenu,menu);
+    }
+
+
+
+
+
+
+
+
 }
